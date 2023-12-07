c5e69435216baace197e529775fdd009beea6a91
yosefha4 <yosefha4@ac.sce.ac.il>
2023-12-07 13:22:00 +0200

    Update 'DeletePostHandler & EditPostHandler'

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -28,7 +28,19 @@ const MyProfile = () => {
   };
   
   const handleDelete = async (post) => {
-  
+    const hasConfirmed = confirm("Are you sure ?");
+
+    if(hasConfirmed){
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`,{
+          method:'DELETE'
+        })
+        const filteredPosts = posts.filter((p) => p._id !==post._id);
+        setPosts(filteredPosts);
+      } catch (error) {
+        console.log(error)
+      }
+    }
   
   };
   
